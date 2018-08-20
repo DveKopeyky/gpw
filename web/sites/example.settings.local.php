5832062cf553564e2cd0beb5d57f27b84385c1c0
Cristian Romanescu <cristian@romanescu.ro>
2018-08-20 14:45:01 +0300

    Updated example.settings.local.php with DB connection

--- web/sites/example.settings.local.php
+++ web/sites/example.settings.local.php
@@ -129,3 +129,15 @@ $settings['rebuild_access'] = TRUE;
  * directory.
  */
 $settings['skip_permissions_hardening'] = TRUE;
+
+
+$databases['default']['default'] = array (
+  'database' => 'unep_gpw',
+  'username' => 'root',
+  'password' => 'root',
+  'host' => 'localhost',
+  'port' => '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+);
