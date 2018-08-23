d93408dff267b8d8ac4ffd882d584a37072f8175
Octavian Belu <beluoctavian@gmail.com>
2018-08-23 08:35:05 +0300

    Video_embed_field.module

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ cd "$SCRIPT_DIR/web"
 ../vendor/bin/drush site:install config_installer -y
 ../vendor/bin/drush user:password admin password
 
+# Site:install command removes write permissions on settings.php file
+chmod u+w sites/default/settings.php
+
