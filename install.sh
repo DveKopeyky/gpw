b295c2be916d927eb93ebec323d56ca7c10dfae9
Octavian Belu <beluoctavian@gmail.com>
2018-08-23 10:48:05 +0300

    Installation script update

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$SCRIPT_DIR/web"
 
 # Site:install command requires write permission on settings.php file
-chmod u+w sites/default/settings.php
+chmod u+w sites/default/ sites/default/settings.php
 
 ../vendor/bin/drush site:install config_installer -y
 ../vendor/bin/drush user:password admin password
 
+# Site:install removes write permission after it's finished
+chmod u+w sites/default/ sites/default/settings.php
