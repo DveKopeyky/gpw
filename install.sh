438e8bcc9b5a8bb86b0d994eb3ebca937e2e479c
Octavian Belu <beluoctavian@gmail.com>
2018-08-23 09:21:45 +0300

    duration_field.module

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$SCRIPT_DIR/web"
 
+# Site:install command requires write permission on settings.php file
+chmod u+w sites/default/settings.php
+
 ../vendor/bin/drush site:install config_installer -y
 ../vendor/bin/drush user:password admin password
-
-# Site:install command removes write permissions on settings.php file
-chmod u+w sites/default/settings.php
 
