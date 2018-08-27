a434ecb768efd9cd717ac3e3f73c5bfdcf392228
Octavian Belu <beluoctavian@gmail.com>
2018-08-27 11:02:08 +0300

    Robo install script

--- install.sh
+++ install.sh
@@ -6,8 +6,12 @@ cd "$SCRIPT_DIR/web"
 # Site:install command requires write permission on settings.php file
 chmod u+w sites/default/ sites/default/settings.php
 
-../vendor/bin/drush site:install config_installer -y
-../vendor/bin/drush user:password admin password
+#../vendor/bin/drush site:install config_installer -y
+#../vendor/bin/drush user:password admin password
+
+cd "$SCRIPT_DIR/robo"
+./robo site:install
+
 
 # Site:install removes write permission after it's finished
 chmod u+w sites/default/ sites/default/settings.php
---
+++ robo/RoboFile.php
@@ -0,0 +1,83 @@
+<?php
+
+// @codingStandardsIgnoreFile
+
+use Robo\Robo;
+use Robo\Tasks;
+
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class RoboFile extends Tasks {
+
+  protected $drush = '../vendor/bin/drush';
+
+  /**
+   * Sync DB from staging server.
+   *
+   * @return null|\Robo\Result
+   * @throws \Robo\Exception\TaskException
+   *
+   * @command sql:sync
+   */
+  public function sqlSync() {
+    $config = Robo::config();
+    $url =  $config->get('project.sync.sql.url');
+    $username = $config->get('project.sync.sql.username');
+    $password = $config->get('project.sync.sql.password');
+    $sql_dump = '../sql/db.sql';
+    $sql_dump_gz = '../sql/db.sql.gz';
+
+    $execStack = $this->taskExecStack()->stopOnFail();
+    $execStack->exec("rm -f $sql_dump $sql_dump_gz");
+    $execStack->exec("curl $url --create-dirs -o $sql_dump_gz -u $username:$password");
+    $execStack->exec("gzip -d $sql_dump_gz");
+    $execStack->exec("{$this->drush} sql:drop -y");
+    $execStack->exec("{$this->drush} sql:query --file=$sql_dump -y");
+    return $execStack->run();
+  }
+
+  /**
+   * Export database dump to a file.
+   *
+   * @param string $destination
+   *  Destination file path.
+   * @param bool $gzip
+   *  If TRUE, the dump will be gzipped.
+   *
+   * @return \Robo\Result
+   *
+   * @command sql:dump
+   */
+  public function sqlDump($destination = 'sites/default/files/sync/database.sql', $gzip = TRUE) {
+    $task = $this->taskExec("{$this->drush} sql:dump --result-file={$destination}");
+    if ($gzip === TRUE) {
+      $task->arg('--gzip');
+    }
+    return $task->run();
+  }
+
+  /**
+   * Install the local instance.
+   *
+   * @return bool|null|\Robo\Result
+   * @throws \Robo\Exception\TaskException
+   *
+   * @command site:install
+   */
+  public function siteInstall() {
+    if ($this->sqlSync()->wasSuccessful()) {
+      $execStack = $this->taskExecStack()->stopOnFail();
+      $execStack->exec("{$this->drush} user:password drupal@eaudeweb.ro password");
+      $execStack->exec("{$this->drush} updatedb -y");
+      $execStack->exec("{$this->drush} entup -y");
+      $execStack->exec("{$this->drush} cim sync -y");
+      $execStack->exec("{$this->drush} cset system.logging error_level verbose -y");
+      $execStack->exec("{$this->drush} cr");
+      return $execStack->run();
+    }
+    return FALSE;
+  }
+}
