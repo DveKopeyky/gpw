356ff37516a8058152afd49e841ac8e8e098c555
Stefan Copetchi <kopezaur@gmail.com>
2018-08-28 13:50:30 +0300

    Implemented custom filters for Meetings(Past meetings and Next meetings)

---
+++ web/modules/custom/gpmeeting/src/Plugin/views/filter/CustomPreviousMeetingsFilter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\gpmeeting\Plugin\views\filter;
+
+use Drupal\Component\Plugin\Exception\PluginException;
+use Drupal\views\Plugin\views\filter\FilterPluginBase;
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
+use Drupal\views\ViewExecutable;
+use Drupal\views\Views;
+
+/**
+ * Filters nodes on current domain_id
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("previous_meetings_filter")
+ */
+class CustomPreviousMeetingsFilter extends FilterPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+    parent::init($view, $display, $options);
+    $this->valueTitle = t('Custom Previous Meetings Filter');
+  }
+
+  public function query() {
+
+    $nu_in_utc = new \DateTime('now', new \DateTimezone('UTC'));
+    $nu_date = $nu_in_utc->format('Y-m-d');
+
+    $configuration = [
+      'table' => 'node__field_date_range',
+      'field' => 'entity_id',
+      'left_table' => 'node_field_data',
+      'left_field' => 'nid',
+      'type' => 'LEFT',
+      'operator' => 'AND'
+    ];
+    try {
+      $join = Views::pluginManager('join')->createInstance('standard', $configuration);
+    } catch (PluginException $exception) {
+      // Do nothing.
+    }
+
+
+    $this->query->addRelationship('node__field_date_range', $join, 'node_field_data');
+    $this->query->addWhere('AND', 'node__field_date_range.field_date_range_value', $nu_date, '<');
+  }
+
+}
+
