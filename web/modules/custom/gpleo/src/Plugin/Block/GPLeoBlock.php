54a83e37995ad80131f4f8c808f191c9c0143009
Cristian Romanescu <cristian@romanescu.ro>
2018-10-18 09:55:26 +0300

    Replaced LEO taxonomy with Thesaurus and other enhancements

--- web/modules/custom/gpleo/src/Plugin/Block/GPLeoBlock.php
+++ web/modules/custom/gpleo/src/Plugin/Block/GPLeoBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\gpleo\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
 use Drupal\taxonomy\Entity\Term;
 
 /**
@@ -20,24 +21,29 @@ class GPLeoBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $vid = 'leo';
+    $vid = 'thesaurus';
     $render = [];
-    $tids = \Drupal::database()
-      ->select('taxonomy_term__field_leo_show', 'a')
-      ->fields('a', ['entity_id'])
-      ->condition('a.bundle', $vid)
-      ->condition('a.field_leo_show_value', 1)
-      ->execute()
-      ->fetchCol();
+    $tids = \Drupal::entityQuery('taxonomy_term')
+      ->condition('vid', $vid)
+      ->condition('field_show_in_term_cloud', 1)
+      ->execute();
     $terms = Term::loadMultiple($tids);
     /** @var \Drupal\taxonomy\TermInterface $term */
     foreach ($terms as $term) {
-      $link = $term->get('field_leo_link')->getString();
+      $link = '';
+      if (!empty($term->get('field_thesaurus_link')->getString())) {
+        try {
+          $link = Url::fromUri($term->get('field_thesaurus_link')->getString());
+          $link = $link->toString();
+        } catch (\Exception $e) {
+          watchdog_exception('GPLeoBlock', $e);
+        }
+      }
       $render[] = [
         'tid' => $term->id(),
         'text' => $term->label(),
         'link' => $link,
-        'importance' => $term->get('field_leo_importance')->getString(),
+        'importance' => $term->get('field_importance')->getString(),
       ];
     }
     return array(
